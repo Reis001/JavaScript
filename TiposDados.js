97eac26ee67322060209d877113c25057ae43fcb
Reis <samuelreisesilva06@gmail.com>
2024-05-02 16:12:35 -0400

    Tipos de dados

---
+++ TiposDados.js
@@ -0,0 +1,14 @@
+let nome = "Wanderson";// Tipo String
+let verdadeiro = true;// Tipo Boolean
+let falso = false;// Tipo Boolean
+let numero = 40;// Tipo Number
+let reais = 25.99;// Tipo Number
+let nulo = null;// Tipo Null
+let indefinido;// Tipo Undefined
+let NotANumber = 0 / 0;// Tipo NaN
+let objeto = {//Tipo Object
+    name: "Wanderson",
+    age: 40,
+    Profession: "Teacher"
+}
+
