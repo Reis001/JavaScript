9b155501dde9252a2068c146ae385f5f0a8e2163
Reis <samuelreisesilva06@gmail.com>
2024-05-02 17:04:02 -0400

    Template literals

---
+++ TemplateLiterals.js
@@ -0,0 +1,6 @@
+let nome = "Wanderson Timóteo";
+let idade = 30;
+
+// Usando template literals para criar uma string com variáveis
+let saudacao = `Ola, meu nome é ${nome} e eu tenho ${idade} anos.` ;
+console.log(saudacao); // Saída: "Olá, meu nome é Wanderson Timóteo e eu tenho 30 anos."
