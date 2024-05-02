b7488406bbcf82a23da51e515e5b6bfa9fb93b5c
Reis <samuelreisesilva06@gmail.com>
2024-05-02 16:58:33 -0400

    desculpa timóteo, os log estavam com espaço

--- Concatenação.js
+++ Concatenação.js
@@ -1,4 +1,4 @@
 let nome = "Wanderson";
 let sobrenome = "Timóteo";
 let resultado = nome + " " + sobrenome;
-console. log(resultado); // Saída: "Wanderson Timóteo"
+console.log(resultado); // Saída: "Wanderson Timóteo"
